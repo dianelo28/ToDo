5da7b293d20df60acb3a200696295ea629635823
dianelo28 <dianelo0228@gmail.com>
2015-07-01 20:20:05 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var ToDo = function (title, description){
-	this.title = title;
-	this.description = description;
-	this.all = [];
-};
-
-ToDo.prototype.save = function(toDo){
-	this.all.push(toDo)
-}
